cb9dab09508e531e40565ad04dfe2c3667b04fcf
Tuure Ilmarinen <tuure.ilmarinen@helsinki.fi>
2018-06-15 14:37:44 +0300

    I would rather be playing SC2.

--- cli.js
+++ cli.js
@@ -4,43 +4,31 @@
 
 const dns = require('dns');
 const got = require('got');
-const ora = require('ora');
-const logUpdate = require('log-update');
 const updateNotifier = require('update-notifier');
 const pkg = require('./package.json');
 
 updateNotifier({pkg}).notify();
 
-const spinner = ora();
 const url = 'http://whatthecommit.com/index.txt';
 const arg = process.argv[2];
 
 if (arg === '-h' || arg === '--help') {
-	console.log(`
- Usage: wtf
-
- Your commit is writing checks your merge can't cash.
- `);
+	console.log("Usage: wtf\nYour commit is writing checks your merge can't cash.");
 	process.exit(1);
 }
 
 dns.lookup('whatthecommit.com', err => {
 	if (err) {
-		logUpdate(`\n› No fucking internet connection!\n`);
+		console.error("No fucking internet connection!");
 		process.exit(1);
-	} else {
-		logUpdate();
-		spinner.text = 'What the actual fuck?';
-		spinner.start();
 	}
 });
 
 got(url).then(res => {
-	logUpdate(`\n›› ${res.body}`);
-	spinner.stop();
+	console.log(res.body);
 }).catch(err => {
 	if (err) {
-		logUpdate(`\n› Motherfuck! \n`);
+		console.error("Motherfuck!");
 		process.exit(1);
 	}
 });
--- readme.md
+++ readme.md
@@ -1,30 +1,25 @@
-<h1 align="center">
-<br>
-<img width="250px" src="https://raw.githubusercontent.com/rishigiridotcom/rishigiri.com/b40bfac606237f61cdc8a5b8e573ce3111721c3d/github/wtf-logo.png">
-<br>
-</h1>
-
 ## Install
 
 ```
-$ npm install --global wtfcommits
+$ npm install --global wtf-am-i-doing
 ```
 
-## Preview
-
-<p align="center">
-<img src="https://raw.githubusercontent.com/rishigiridotcom/rishigiri.com/f19779bf80f46cab18cb947b6c69a73a7e6ed1f7/github/wtfcommits.gif">
-</p>
 
 ## Usage
 
 ```
-$ wtf
+$ wtf-am-i-doing
 ```
+```
+$ git commit -am"`wtf-am-i-doing`"
+```
+
 
 ## Related
 
 - __[`what-the-commit`](https://github.com/CodeDotJS/what-the-commit)__ `:` `An API to generate random commit messages!`
+- __[`wtfcommits`](https://github.com/CodeDotJS/wtfcommits)__ `:` ` Motherfucking commits straight from the goddamn console.`
+
 
 ## License
 
